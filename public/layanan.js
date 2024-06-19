fbc98ffac9bc69c90298fad766fa413612e23e85
Annasaby <akifsakti9@gmail.com>
2024-06-19 15:32:14 +0700

    menambahkan file berita dan galeri

--- public/layanan.js
+++ public/layanan.js
@@ -37,13 +37,11 @@ function sendLayanan(){
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "tabgemblung@gmail.com",
-        // Password : "100C7D3FF26FCD6318C9AC9E1197AB697EAA",
         Password : "FFA39B1652DFBBE80520BF14154A57DE7A6C",
         To : 'akifsakti9@gmail.com',
-        From : "asada@gmail.com",
+        From : "akifsakti9@gmail.com",
         Subject : "Form Layanan Terpadu",
-        Body : "halohalo"
-        // Body : "Tujuan pelaporan: " + document.getElementById("tujuan").value + "<br> Nama lengkap: " + document.getElementById("nama").value + "<br> Phone: " + document.getElementById("phone").value + "<br> E-Mail: " + document.getElementById("email").value + "<br> Judul laporan: " + document.getElementById("judul").value + "<br> Deskripsi laporan: " + document.getElementById("deskripsi").value + "<br> Tanggal kejadian: " + document.getElementById("tanggal").value + "<br> Lokasi: " + document.getElementById("lokasi").value + "<br> Lampiran: " + document.getElementById("Lampiran").value
+        Body : "Tujuan pelaporan: " + document.getElementById("tujuan").value + "<br> Nama lengkap: " + document.getElementById("nama").value + "<br> Phone: " + document.getElementById("phone").value + "<br> E-Mail: " + document.getElementById("email").value + "<br> Judul laporan: " + document.getElementById("judul").value + "<br> Deskripsi laporan: " + document.getElementById("deskripsi").value + "<br> Tanggal kejadian: " + document.getElementById("tanggal").value + "<br> Lokasi: " + document.getElementById("lokasi").value + "<br> Lampiran: " + document.getElementById("Lampiran").value
     }).then(
       message => alert(message)
     );
